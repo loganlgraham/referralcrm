50797e32aa3b323b476f9f33467294bb6a506fd9
Logan Graham <loganlgraham@gmail.com>
2025-11-04 21:51:17 -0700

    new file:   src/components/providers/next-auth-provider.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 import { ReactNode } from 'react';
 import { Toaster } from 'sonner';
 import { Inter } from 'next/font/google';
-import { SessionProvider } from 'next-auth/react';
+import { NextAuthProvider } from '@/components/providers/next-auth-provider';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -18,9 +18,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.variable}>
       <body className="min-h-screen bg-slate-100 text-slate-900">
         <Toaster position="top-right" richColors closeButton />
-        <SessionProvider>
+        <NextAuthProvider>
           {children}
-        </SessionProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
