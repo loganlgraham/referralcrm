5d9396361a895edf148a882b9894cede5f20636d
loganlgraham <loganlgraham@gmail.com>
2025-12-11 18:09:16 -0700

    Update brand color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,9 +15,9 @@ const config: Config = {
       },
       colors: {
         brand: {
-          DEFAULT: '#0F4C81',
-          light: '#2f6aa3',
-          dark: '#0b365d'
+          DEFAULT: '#4EA66D',
+          light: '#6FBE8B',
+          dark: '#3B7E56'
         }
       }
     }
